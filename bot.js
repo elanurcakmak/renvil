04c5f7b0fb05fd6256012e6e6ed770d635a87ece
elanurcakmak <72741369+elanurcakmak@users.noreply.github.com>
2020-12-16 14:10:44 +0300

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,227 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const ayarlar = require('./ayarlar.json');
+const chalk = require('chalk');
+const fs = require('fs');
+const db = require('wio.db');
+const http = require('http');
+const express = require('express');
+const moment = require("moment");
+const app = express();
+require("moment-duration-format");
+app.get("/", (request, response) => {
+console.log("ArdaDemr | Hostlandı");
+response.sendStatus(200);
+});
+app.listen(8000);
+setInterval(() => {
+http.get(`http://ozelfyukas.glitch.me/`);
+}, 280000)
+require('./util/eventLoader')(client);
+
+var prefix = ayarlar.prefix;
+
+const log = message => {
+  console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] ${message}`);
+};
+
+client.commands = new Discord.Collection();
+client.aliases = new Discord.Collection();
+fs.readdir('./komutlar/', (err, files) => {
+  if (err) console.error(err);
+  log(`${files.length} komut yüklenecek.`);
+  files.forEach(f => {
+    let props = require(`./komutlar/${f}`);
+    log(`Yüklenen komut: ${props.help.name}.`);
+    client.commands.set(props.help.name, props);
+    props.conf.aliases.forEach(alias => {
+      client.aliases.set(alias, props.help.name);
+    });
+  });
+});
+
+client.reload = command => {
+  return new Promise((resolve, reject) => {
+    try {
+      delete require.cache[require.resolve(`./komutlar/${command}`)];
+      let cmd = require(`./komutlar/${command}`);
+      client.commands.delete(command);
+      client.aliases.forEach((cmd, alias) => {
+        if (cmd === command) client.aliases.delete(alias);
+      });
+      client.commands.set(command, cmd);
+      cmd.conf.aliases.forEach(alias => {
+        client.aliases.set(alias, cmd.help.name);
+      });
+      resolve();
+    } catch (e){
+      reject(e);
+    }
+  });
+};
+
+client.load = command => {
+  return new Promise((resolve, reject) => {
+    try {
+      let cmd = require(`./komutlar/${command}`);
+      client.commands.set(command, cmd);
+      cmd.conf.aliases.forEach(alias => {
+        client.aliases.set(alias, cmd.help.name);
+      });
+      resolve();
+    } catch (e){
+      reject(e);
+    }
+  });
+};
+
+client.unload = command => {
+  return new Promise((resolve, reject) => {
+    try {
+      delete require.cache[require.resolve(`./komutlar/${command}`)];
+      let cmd = require(`./komutlar/${command}`);
+      client.commands.delete(command);
+      client.aliases.forEach((cmd, alias) => {
+        if (cmd === command) client.aliases.delete(alias);
+      });
+      resolve();
+    } catch (e){
+      reject(e);
+    }
+  });
+};
+
+
+var regToken = /[\w\d]{24}\.[\w\d]{6}\.[\w\d-_]{27}/g;
+// client.on('debug', e => {
+//   console.log(chalk.bgBlue.green(e.replace(regToken, 'that was redacted')));
+// });
+
+client.on('warn', e => {
+  console.log(chalk.bgYellow(e.replace(regToken, 'that was redacted')));
+});
+
+client.on('error', e => {
+  console.log(chalk.bgRed(e.replace(regToken, 'that was redacted')));
+});
+
+client.login(ayarlar.token);
+
+//---------------------------------komutlar---------------------------------\\
+client.on("guildMemberAdd", async member => {
+if (!member.user.bot) return;
+await member.guild.ban(member.guild.member(member))
+})
+
+//---------------------------------DDOS KORUMASI-----------------------------\\
+client.on('message', msg => {
+
+if(client.ping > 2500) {
+
+            let bölgeler = ['singapore', 'eu-central', 'india', 'us-central', 'london',
+            'eu-west', 'amsterdam', 'brazil', 'us-west', 'hongkong', 
+            'us-south', 'southafrica', 'us-east', 'sydney', 'frankfurt',
+            'russia']
+           let yenibölge = bölgeler[Math.floor(Math.random() * bölgeler.length)]
+           let sChannel = msg.guild.channels.find(c => c.name === "ddos-system")
+
+           sChannel.send(`Sunucu'ya Vuruyorlar \nSunucu Bölgesini Değiştirdim \n __**${yenibölge}**__ :tik: __**Sunucu Pingimiz**__ :`+ client.ping)
+           msg.guild.setRegion(yenibölge)
+           .then(g => console.log(" bölge:" + g.region))
+           .then(g => msg.channel.send("bölge **"+ g.region  + " olarak değişti")) 
+           .catch(console.error);
+}});
+//---------------------------------DDOS KORUMASI-----------------------------\\
+
+client.on("roleDelete", async(role , channel , message , guild) => {
+  let rolkoruma = await db.fetch(`rolk_${role.guild.id}`);
+    if (rolkoruma == "acik") {
+  role.guild.createRole({name: role.name, color: role.color,  permissions: role.permissions}) 
+        role.guild.owner.send(` **${role.name}** Adlı Rol Silindi Ve Ben Rolü Tekrar Oluşturdum  :white_check_mark::`)
+
+  
+}
+})  
+
+//--------------------KANAL KORUMA--------------------------------\\
+client.on("channelDelete", async channel => {
+  const logs = await channel.guild.fetchAuditLogs({ type: 'CHANNEL_DELETE' }).then(audit => audit.entries.first())
+  const deleter = await channel.guild.members.get(logs.executor.id);
+  if(deleter.id == "") return; //bu satıra kendi id'nizi yazın sizin kanal silmenizi engellemeyecektir
+  channel.clone(undefined, true, true, "Kanal silme koruması sistemi").then(async klon => {
+    await klon.setParent(channel.parent);
+    await klon.setPosition(channel.position);
+  })
+})
+
+///küfür///
+client.on("message", async msg => {
+  if (msg.author.bot) return;
+  if (msg.channel.type === "dm") return;
+
+  let i = await db.fetch(`küfürFiltre_${msg.guild.id}`);
+  if (i == "acik") {
+    const küfür = [
+      "amcık",
+      "yarrak",
+      "orospu",
+      "oç",
+      "piç",
+      "sikerim",
+      "sikik",
+      "amına",
+      "pezevenk",
+      "yavşak",
+      "ananı",
+      "anandır",
+      "orospu",
+      "evladı",
+      "göt",
+      "pipi",
+      "sokuk",
+      "yarak",
+      "bacını",
+      "karını",
+      "amk",
+      "aq",
+      "mk",
+      "anaskm"
+    ];
+    if (küfür.some(word => msg.content.toLowerCase().includes(word))) {
+      try {
+        if (!msg.member.hasPermission("MANAGE_WEBHOOKS")) {
+          msg.delete();
+          let embed = new Discord.RichEmbed()
+            .setColor(0xffa300)
+            .setFooter("Küfür Sistemi", client.user.avatarURL)
+            .setAuthor(
+              msg.guild.owner.user.username,
+              msg.guild.owner.user.avatarURL
+            )
+            .setDescription(
+              "PinkCode, " +
+                `***${msg.guild.name}***` +
+                " adlı sunucunuzda küfür yakaladım."
+            )
+            .addField(
+              "Küfür Eden Kişi",
+              "Kullanıcı: " + msg.author.tag + "\nID: " + msg.author.id,
+              true
+            )
+            .addField("Engellenen mesaj", msg.content, true)
+            .setTimestamp();
+          msg.guild.owner.user.send(embed);
+          return msg.channel
+            .send(
+              `${msg.author}, Küfür Etmek Yasak! Senin Mesajını Özelden Kurucumuza Gönderdim.`
+            )
+            .then(msg => msg.delete(25000));
+        }
+      } catch (err) {
+        console.log(err);
+      }
+    }
+  }
+  if (!i) return;
+});
+
